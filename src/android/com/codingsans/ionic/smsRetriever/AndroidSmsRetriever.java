3d36ad0c823b890dcf83dab71668d3942f3a099f
Muhammad Awais <awais.sds@gmail.com>
2020-10-26 08:45:28 +0500

    remove NonNull exception

--- src/android/com/codingsans/ionic/smsRetriever/AndroidSmsRetriever.java
+++ src/android/com/codingsans/ionic/smsRetriever/AndroidSmsRetriever.java
@@ -11,7 +11,7 @@ import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+//import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -156,14 +156,14 @@ public class AndroidSmsRetriever extends CordovaPlugin {
         }
       });
 
-      task.addOnFailureListener(new OnFailureListener() {
+      /*task.addOnFailureListener(new OnFailureListener() {
         @Override
         public void onFailure(@NonNull Exception e) {
           // Failed to start retriever, inspect Exception for more details
           LOG.v(TAG, "Executing action: addOnFailureListener");
           //Toast.makeText(cordova.getActivity().getApplicationContext(),"Executing action: addOnFailureListener", Toast.LENGTH_SHORT).show();
         }
-      });
+      });*/
 
       PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
       r.setKeepCallback(true);
